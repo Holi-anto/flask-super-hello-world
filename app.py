36bb08a51d98a0ef3ac647f72f56d00b0dee887f
Antonia Jimenez <antonia.jimenez@soymas.cl>
2023-06-25 18:41:47 -0400

    app

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
+    
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html', user = request.args.get('username'))
